1ff5634f1bf499244597c642e8e9ffb9c3dfbf9e
Tasneem Obeid <tasneemo@umich.edu>
2021-03-09 13:22:41 -0500

    commit

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 17;
-        int second = 27;
+        int first = 18;
+        int second = 28;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second + 5;
