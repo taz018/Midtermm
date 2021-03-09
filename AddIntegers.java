935090f284f26d6425c499a8750a386d29ebb652
Tasneem Obeid <tasneemo@umich.edu>
2021-03-09 13:14:55 -0500

    Atom commit

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 15;
-        int second = 25;
+        int first = 17;
+        int second = 27;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second + 5;
